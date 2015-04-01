620f9d241bace226ce4c6db7f54c76754cfab6b8
Karvari Ellingson <mzphitt@gmail.com>
2015-04-01 16:51:09 -0700

    Update Gemfile; establish root in routes; flesh out spec.

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,13 +8,15 @@ private
       response = RestClient::Request.new(
         :method => :post,
         :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
-        :user =>  ENV['TWILIO_ACCOUNT_SID']
-        :password =>  ENV['TWILIO_AUTH_TOKEN']
+        :user =>  ENV['TWILIO_ACCOUNT_SID'],
+        :password =>  ENV['TWILIO_AUTH_TOKEN'],
         :payload => { :Body => body,
                       :From => from,
                       :To => to }
       ).execute
-    rescue
+    rescue RestClient::BadRequest => error
+      message = JSON.parse(error.response)['message']
+      errors.add(:base, message)
       false
     end
   end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
-describe Message do
-  it "" do
-    message = Message.new(:body => 'hello', :to => '5555555', :from => '5555555555')
+describe Message, :vcr => true do
+  it "doesn't save the message if twilio gives an error" do
+    message = Message.new(:body => 'hi', :to => '1111111', :from => '5039463641')
     message.save.should be false
+  end
+
+  it "adds an error if the to number is invalid" do
+    message = Message.new(:body => 'hello', :to => '5555555', :from => '5034107773')
+    message.save
+    message.errors.messages[:base].should eq ["The 'To' number 5555555 is not a valid phone number"]
   end
 end
